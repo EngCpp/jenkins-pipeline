0f794cb8a8bfc82a95a02a35642d65ea766d7251
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-28 21:26:12 +1300

    Update buildCascade.groovy

--- vars/buildCascade.groovy
+++ vars/buildCascade.groovy
@@ -1,3 +1,17 @@
-def call() {
-  echo "Build cascade"
+def call(cascades = []) {
+  stage('cascade') {
+    for (String cascade : cascades) {
+      try {
+        def breakout = cascade.tokenize('/')
+        if (breakout[1]) {
+	        build job: "${breakout[0]}/${breakout[1]}", wait: false
+        } else {
+	        build job: "${cascade}/${BRANCH_NAME}", wait: false
+        }
+      }
+      catch (failure) {
+        echo "Failed to cascade ${cascade}/${BRANCH_NAME}"
+      }
+    }
+  }
 }
