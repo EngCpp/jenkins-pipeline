3e2d9da8a3a084861d6cba3d3ba01b4748637d5f
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-28 23:07:18 +1300

    Create groupId.groovy

---
+++ vars/groupId.groovy
@@ -0,0 +1,9 @@
+def call(pom) {
+    if (BRANCH_NAME.startsWith('master'))
+        return pom.groupId.replace(".unstable.",".stable.")
+
+    if (BRANCH_NAME.startsWith('develop'))
+        return pom.groupId.replace(".stable.",".unstable.")
+
+    return pom.groupId.replace(".stable.",".canary.").replace(".unstable.", ".canary.")
+}
