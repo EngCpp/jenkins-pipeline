eb5ec6ac4c95f698af34a3435337d25fdc4ad182
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-29 22:00:57 +1300

    Update prepareVersion.groovy

--- vars/prepareVersion.groovy
+++ vars/prepareVersion.groovy
@@ -1,23 +1,28 @@
 def call() {
     log.info "PREPARE VERSION"
     stage('prepare') {
-        milestone()
-        log.info "READING POM"
-        def pom = readMavenPom file: 'pom.xml'
-        log.info "VERSION ${pom.version}"
-        def contractVersion = pom.version.split(".999")[0]
-        def tagPrefix = "${pom.artifactId}-${contractVersion}"
+        try {
+            milestone()
+            log.info "READING POM"
+            def pom = readMavenPom file: 'pom.xml'
+            log.info "VERSION ${pom.version}"
+            def contractVersion = pom.version.split(".999")[0]
+            def tagPrefix = "${pom.artifactId}-${contractVersion}"
 
-        log.info "From ${pom.version} on ${env.BRANCH_NAME} we have prefix ${tagPrefix}"
-        def nextTag = nextTag(tagPrefix)
-        pom.version = nextTag.tokenize('-').last() 
+            log.info "From ${pom.version} on ${env.BRANCH_NAME} we have prefix ${tagPrefix}"
+            def nextTag = nextTag(tagPrefix)
+            pom.version = nextTag.tokenize('-').last() 
 
-        pom.groupId = groupId(pom)
-        
-        writeMavenPom model: pom
-        
-        currentBuild.displayName = "${pom.version} #${currentBuild.number}"
-        
-        return nextTag
+            pom.groupId = groupId(pom)
+
+            writeMavenPom model: pom
+
+            currentBuild.displayName = "${pom.version} #${currentBuild.number}"
+
+            return nextTag
+        } catch (Exception e) {
+            log.info "ERROR: ${e}"
+            throw e;
+        }        
     }
 }
