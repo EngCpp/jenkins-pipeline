6206de12a0b91c19ce28403f9596ea122df7e1b5
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-29 22:24:33 +1300

    Update prepareVersion.groovy

--- vars/prepareVersion.groovy
+++ vars/prepareVersion.groovy
@@ -4,14 +4,18 @@ def call() {
         try {
             milestone()
             log.info "READING POM"
+            
             def pom = readMavenPom file: 'pom.xml'
+            
             log.info "VERSION ${pom.version}"
+            
             def contractVersion = pom.version.split(".999")[0]
             def tagPrefix = "${pom.artifactId}-${contractVersion}"
+            
             log.info "From ${pom.version} on ${env.BRANCH_NAME} we have prefix ${tagPrefix}"
+            
             def nextTag = nextTag(tagPrefix)
             pom.version = nextTag.tokenize('-').last() 
-
             pom.groupId = groupId(pom)
 
             writeMavenPom model: pom
