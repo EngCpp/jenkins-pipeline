4d16dadd427900b27e82351b3f3e014ac7a0c49a
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-29 22:23:57 +1300

    Update prepareVersion.groovy

--- vars/prepareVersion.groovy
+++ vars/prepareVersion.groovy
@@ -8,8 +8,7 @@ def call() {
             log.info "VERSION ${pom.version}"
             def contractVersion = pom.version.split(".999")[0]
             def tagPrefix = "${pom.artifactId}-${contractVersion}"
-
-            //log.info "From ${pom.version} on ${env.BRANCH_NAME} we have prefix ${tagPrefix}"
+            log.info "From ${pom.version} on ${env.BRANCH_NAME} we have prefix ${tagPrefix}"
             def nextTag = nextTag(tagPrefix)
             pom.version = nextTag.tokenize('-').last() 
 
