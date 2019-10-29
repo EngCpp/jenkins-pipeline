cdaaa83902ee45fa88ae1497bba0db96b1e3bd2f
Carlos Eduardo Rodrigues dos Santos <engcpp@yahoo.com.br>
2019-10-29 21:45:42 +1300

    Update groupId.groovy

--- vars/groupId.groovy
+++ vars/groupId.groovy
@@ -1,10 +1,10 @@
 def call(pom) {
     log.info "GROUP ID"
     
-    if (BRANCH_NAME.startsWith('master'))
+    if (env.BRANCH_NAME.startsWith('master'))
         return pom.groupId.replace(".unstable.",".stable.")
 
-    if (BRANCH_NAME.startsWith('develop'))
+    if (env.BRANCH_NAME.startsWith('develop'))
         return pom.groupId.replace(".stable.",".unstable.")
 
     return pom.groupId.replace(".stable.",".canary.").replace(".unstable.", ".canary.")
